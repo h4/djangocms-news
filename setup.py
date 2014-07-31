40735def4ecda362079cf1331c011b319c092e38
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-08-01 00:39:10 +0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='djangocms-news',
+    version='0.1.0',
+    packages=['news', 'news.migrations'],
+    url='https://github.com/h4/djangocms-news',
+    license='MIT',
+    author='Mikhail Baranov',
+    author_email='mkhl@brnv.ru',
+    description='News app',
+    install_requires=[
+        'django-cms>=3.0.0',
+        'south>=0.7.2',
+        'django-ckeditor-updated>=4.2.8',
+        'django-model-utils>=2.0.0',
+        'django-filer>=0.9.7',
+        'django-hvad>=0.4.1',
+    ],
+)
