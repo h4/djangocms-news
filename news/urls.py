25fbce85869ecfd53a8ca973e3b286ca7862f16e
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-09-08 01:58:30 +0400

    Add trailing slashes to urls

--- news/urls.py
+++ news/urls.py
@@ -4,9 +4,9 @@ from news.views import NewsListView, NewsCategoryView, NewsYearArchiveView, News
 
 urlpatterns = patterns('news',
    url(r'^$', NewsListView.as_view(), name='news_index'),
-   url(r'^category/(?P<category_id>\d+)$', NewsCategoryView.as_view(), name='news_archive_year'),
-   url(r'^(?P<year>\d{4})$', NewsYearArchiveView.as_view(), name='news_archive_year'),
-   url(r'^(?P<year>\d{4})/(?P<month>\d{2})$', NewsMonthArchiveView.as_view(), name='news_archive_month'),
-   url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$', NewsDayArchiveView.as_view(), name='news_archive_day'),
-   url(r'^(?P<pk>\d+)-(?P<day>\d{2})(?P<month>\d{2})(?P<year>\d{4})$', NewsItemView.as_view(), name='news_item'),
+   url(r'^category/(?P<category_id>\d+)/$', NewsCategoryView.as_view(), name='news_archive_year'),
+   url(r'^(?P<year>\d{4})/$', NewsYearArchiveView.as_view(), name='news_archive_year'),
+   url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', NewsMonthArchiveView.as_view(), name='news_archive_month'),
+   url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', NewsDayArchiveView.as_view(), name='news_archive_day'),
+   url(r'^(?P<pk>\d+)-(?P<day>\d{2})(?P<month>\d{2})(?P<year>\d{4})/$', NewsItemView.as_view(), name='news_item'),
 )
