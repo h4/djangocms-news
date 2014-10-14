ff4ae633f94e38d6b1321f986a729cadebc16956
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-10-14 12:16:57 +0400

    Добавил опцию «выводить обложку»

--- news/models.py
+++ news/models.py
@@ -86,3 +86,4 @@ class LatestNewsPlugin(CMSPlugin):
     category = models.ForeignKey(Category, verbose_name=_('category'), null=True, blank=True)
     limit = models.PositiveIntegerField(_('Number of news to show'),
                                         help_text=_('Limits the number of news that will be displayed'))
+    show_cover = models.BooleanField(_('Show cower'), default=True)
